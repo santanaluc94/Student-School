f783d1e11301505bc6e9246054959e05bade2595
Lucas Santana <santanaluc94@gmail.com>
2019-05-15 00:13:11 -0300

    Footer layout finalized

--- app/view/guest/footer.php
+++ app/view/guest/footer.php
@@ -1,34 +1,43 @@
 
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+
+
+
 <footer class="footer">
 	<div class="container">
 		<div class="container bottom_border">
 			<div class="row">
 				<div class=" col-sm-4 col-md col-sm-4  col-12 col">
-					<h5 class="headin5_amrc col_white_amrc pt2">Find us</h5>
-					<!--headin5_amrc-->
-					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. We are an award-winning creative agency, dedicated to the best result in web design, promotion, business consulting, and marketing.</p>
+					<h5 class="title_footer">Find us</h5>
+					<p>
+						Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+						Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+						We are an award-winning creative agency, dedicated to the best result in web 
+						design, promotion, business consulting, and marketing.
+					</p>
 				</div>
 				<div class=" col-sm-4 col-md col-sm-4  col-12 col">
-					<h5 class="headin5_amrc col_white_amrc pt2">Newsletter</h5>
+					<h5 class="title_footer">Newsletter</h5>
 					<p>A rover wearing a fuzzy suit doesn’t alarm the real penguins</p>
-					<div class="input-group">
-						<input type="text" class="form-control" placeholder="Search for..." />
-						<span class="input-group-btn">
-							<button class="btn btn-default" type="button"></button>
-						</span>
+					<div class="input-group mb-3">
+						<input type="text" class="form-control" />
+						<div class="input-group-append">
+							<span class="input-group-btn">
+								<button class="btn btn-secondary" type="button">Subscribe</button>
+							</span>
+						</div>
 					</div>
 					<!-- /input-group -->
 				</div>
 				<div class=" col-sm-4 col-md col-sm-4  col-12 col">
-					<h5 class="headin5_amrc col_white_amrc pt2">Contact Us</h5>
-					<!--headin5_amrc-->
+					<h5 class="title_footer">Contact Us</h5>
 					<p><span class="test">Address:</span> 38 Street  Particular II, Guarulhos, SP</p>
 					<p><span class="test">Phone:</span> +55 (11) 2404-0992 or +55 (11) 9 6294-2356</p>
 					<p><span class="test">E-mail:</span> santanaluc94@gmail.com</p>
 					<!--footer_ul_amrc ends here-->
 				</div>
 			</div>
-			<ul class="foote_bottom_ul_amrc">
+			<ul class="nav_bottom">
 				<li><a href="?page=index">Home</a></li>
 				<li><a href="?page=about">About</a></li>
 				<li><a href="?page=courses">Services</a></li>
@@ -38,7 +47,7 @@
 			</ul>
 			<!--foote_bottom_ul_amrc ends here-->
 			<p class="text-center">Copyright @2017 | Designed With by <a href="#">Your Company Name</a></p>
-			<ul class="social_footer_ul">
+			<ul class="social_icons">
 				<li><a href="http://webenlance.com"><i class="fab fa-facebook-f"></i></a></li>
 				<li><a href="http://webenlance.com"><i class="fab fa-twitter"></i></a></li>
 				<li><a href="http://webenlance.com"><i class="fab fa-linkedin"></i></a></li>
