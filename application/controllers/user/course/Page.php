aa17762dea9d26007a36a66e8a7dd6b828a295d1
Lucas Santana <santanaluc94@gmail.com>
2020-04-13 22:41:13 -0300

    Creating page course

--- application/controllers/user/course/Page.php
+++ application/controllers/user/course/Page.php
@@ -1,27 +1,31 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Login extends CI_Controller
+class Page extends CI_Controller
 {
 
     public function __construct()
     {
         parent::__construct();
         $this->load->library('session');
-        $this->load->model('users');
+        $this->load->model('courses');
         $this->load->helper('session_helper');
         $this->load->helper('user_data_helper');
     }
 
     public function index(): void
     {
+        $id = (int) $_GET['id'];
+
+
         if (hasSession()) {
             $data = get_object_vars($_SESSION['userData']);
             $data = formatUserData($data);
+            $data['course'] = $this->courses->getCourseById($id);
 
-            redirect("/user/dashboard", $data);
+            $this->template->show("/user/course/page", $data);
+        } else {
+            redirect('guest/login');
         }
-
-        $this->template->show('guest/login');
     }
 }
---
+++ application/views/admin/header.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Student School</title>
+
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Bootstrap 4 Mobile App Template">
+    <link rel="shortcut icon" href="favicon.ico">
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Maven+Pro:400,500,700|Nunito:400,600,700" rel="stylesheet">
+
+    <!-- FontAwesome JS-->
+    <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js" integrity="sha384-0pzryjIRos8mFBWMzSSZApWtPl/5++eIfzYmTgBBmXYdhvxPc+XcFEk+zJwDgWbP" crossorigin="anonymous">
+    </script>
+
+    <!-- Plugins CSS -->
+    <link rel="stylesheet" href="<?= base_url('public/jquery_flipster_master/dist/jquery.flipster.min.css'); ?>">
+
+    <!-- Theme CSS -->
+    <link id="theme-style" rel="stylesheet" href="<?= base_url('public/css/theme.css'); ?>">
+    <link id="theme-style" rel="stylesheet" href="<?= base_url('public/css/styles.css'); ?>">
+    <link id="theme-style" rel="stylesheet" href="<?= base_url('public/css/boleto/boleto_styles.css'); ?>">
+
+</head>
+
+<body>
+    <header class="header">
+        <div class="branding">
+            <div class="container position-relative">
+                <nav class="navbar navbar-expand-lg">
+                    <h1 class="site-logo">
+                        <a class="navbar-brand" href="<?= base_url('') ?>">
+                            <img class="logo-icon" src="<?= base_url('public/images/logo-icon.svg'); ?>" alt="logo">
+                            <!-- Logo -->
+                            <span class="logo-text">Student School</span>
+                        </a>
+                    </h1>
+
+                    <ul class="social-list list-inline mb-0 position-absolute">
+                        <li class="list-inline-item">
+                            <a class="nav-link" href="<?= base_url('user/dashboard'); ?>">Dashboard</a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a class="nav-link" href="<?= base_url('user/my-courses'); ?>">My Courses</a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a class="nav-link" href="<?= base_url('user/account/profile'); ?>">Profile</a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a class="nav-link" href="<?= base_url('user/search'); ?>">Search</a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a class="nav-link" href="<?= base_url('user/dashboard/logOut'); ?>">Log Out</a>
+                        </li>
+                    </ul>
+                    <!-- Menu -->
+                </nav>
+            </div>
+        </div>
+    </header>
+    <!--Header-->
