d297060202fbbcb78f0883ba43c542ead713590d
Jane Tutume <janenataly12@gmail.com>
2020-05-13 19:51:16 -0300

    fixed form register

--- application/views/guest/login.php
+++ application/views/guest/login.php
@@ -7,13 +7,13 @@
                     <div class="card-body">
                         <form action="<?= base_url('guest/loginPost/execute') ?>" method="post" id="login_form">
                             <div class="form-group row">
-                                <label for="email" class="col-md-4 col-form-label text-md-right text-login">E-Mail Address</label>
+                                <label for="email" class="col-md-4 col-form-label text-md-right text-form">E-Mail Address</label>
                                 <div class="col-md-6">
                                     <input type="email" id="email" class="box-field form-control" name="email" placeholder="example@email.com" required autofocus>
                                 </div>
                             </div>
                             <div class="form-group row">
-                                <label for="password" class="col-md-4 col-form-label text-md-right text-login">Password</label>
+                                <label for="password" class="col-md-4 col-form-label text-md-right text-form">Password</label>
                                 <div class="col-md-6">
                                     <input type="password" id="password" class="box-field form-control" name="password" required>
                                 </div>
@@ -21,8 +21,8 @@
                             <div class="form-group row">
                                 <div class="col-md-6 offset-md-4">
                                     <div class="checkbox">
-                                        <label class="text-login">
-                                            <input class="text-login" type="checkbox" name="remember"> Remember Me
+                                        <label class="text-form">
+                                            <input class="text-form" type="checkbox" name="remember"> Remember Me
                                         </label>
                                     </div>
                                 </div>
@@ -37,7 +37,7 @@
                                 <div>
                                     <span class="help-block"></span>
                                 </div>
-                                <a href="<?= base_url('guest/forgotpassword'); ?>" class="btn btn-link text-login">
+                                <a href="<?= base_url('guest/forgotpassword'); ?>" class="btn btn-link text-form">
                                     Forgot Your Password?
                                 </a>
                             </div>
