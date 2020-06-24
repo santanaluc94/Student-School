58796f51c54d0ea3d79d3602aef515fc0e88216e
Jane Nataly <janenataly12@gmail.com>
2020-06-24 15:20:46 -0300

    Fixed delete page

--- application/views/user/account/delete.php
+++ application/views/user/account/delete.php
@@ -1,6 +1,8 @@
 <main class="content">
     <div class="container">
-        <h1>Profile Page</h1>
+        <div class="box-title-page">
+            <h1 class="title-page">Profile Page</h1>
+        </div>
 
         <div class="row justify-content-center">
             <div class="col-md-3">
@@ -13,20 +15,20 @@
 
             <!-- Delete Profile -->
             <div class="col-md-9">
-                <div class="card">
-                    <div class="card-header">Delete Account</div>
+                <div class="card box-card">
+                    <div class="card-header head-card">Delete Account</div>
                     <div class="card-body">
                         <form action="<?= base_url('user/account/deletePost/delete') ?>" method="post" onsubmit="return checkForm()">
-                            <div class="form-group row" style="display: none">
+                            <div class="form-group row " style="display: none">
                                 <label for="id" class="col-md-4 col-form-label text-md-right">User Id</label>
                                 <div class="col-md-6">
                                     <input value="<?= $id ?>" type="number" id="id" class="form-control" name="id" required />
                                 </div>
                             </div>
-                            <p>This proccess can not be reverted. Are you sure that you want do delete your account?</p>
+                            <p class="text-form">This proccess can not be reverted. Are you sure that you want do delete your account?</p>
                             <div class="form-check">
-                                <input type="checkbox" class="form-check-input" id="deleteAccount" name="deleteAccount">
-                                <label class="form-check-label" for="deleteAccount">Yes, I want to delete my account.</label>
+                                <input type="checkbox" class="form-check-input box-field" id="deleteAccount" name="deleteAccount">
+                                <label class="form-check-label text-form" for="deleteAccount">Yes, I want to delete my account.</label>
                             </div>
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-danger">
